47c93a21b49141670c1571055850762d85b5f1d8
pamsfields <jh5811zj@go.minneapolis.edu>
2018-01-22 12:37:23 -0600

    added readdate automatically when book marked as read

--- book.py
+++ book.py
@@ -2,15 +2,15 @@ class Book:
 
     ''' Represents one book in a user's list of books'''
     '''need to import datetime'''
-    import datetime
 
     NO_ID = -1
 
-    def __init__(self, title, author, read=False, id=NO_ID):
+    def __init__(self, title, author, read=False, read_date=None, id=NO_ID):
         '''Default book is unread, and has no ID'''
         self.title = title
         self.author = author
         self.read = read
+        self.read_date = read_date
         self.id=id
 
 
@@ -21,15 +21,19 @@ class Book:
     def __str__(self):
         read_str = 'no'
         if self.read:
+            import datetime
             read_str = 'yes'
+            read_date = datetime.datetime.now()
+            self.read_date = read_date.strtime('%m/%d/%Y')
 
         id_str = self.id
         if id == -1:
             id_str = '(no id)'
 
-        template = 'id: {} Title: {} Author: {} Read: {}'
-        return template.format(id_str, self.title, self.author, read_str)
+
+        template = 'id: {} Title: {} Author: {} Read: {} ReadDate: {}'
+        return template.format(id_str, self.title, self.author, read_str, self.read_date)
 
 
     def __eq__(self, other):
-        return self.title == other.title and self.author == other.author and self.read == other.read and self.id==other.id
+        return self.title == other.title and self.author == other.author and self.read == other.read and self.id==other.id and self.read_date == other.read_date
